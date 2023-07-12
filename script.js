d81a0764674f6504a605fb566f467771461f724b
bhargavsai888 <117674796+bhargavsai888@users.noreply.github.com>
2023-07-12 13:10:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function calculateMinCost() {
   //your code here
+	var str=document.getElementById('rope-lengths').value;
   
-  
+  var arr=str.split(',');
+	while(arr.length>1){
+		arr.sort();
+		var a=arr.shift();
+		var b=arr.shift();
+		var c=a+b;
+		arr.unshift(c);
+	}
+	var final=arr.join();
+	console.log(final);
+	document.getElementById('result').value=final;
   
 }  
