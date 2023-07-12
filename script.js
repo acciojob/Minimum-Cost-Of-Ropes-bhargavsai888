f8a7f041c5add9d35417522d83f949de02049cd5
bhargavsai888 <117674796+bhargavsai888@users.noreply.github.com>
2023-07-12 15:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,27 +8,25 @@ function calculateMinCost() {
   return parseInt(str);
   });
   var crr=[];
-	//brr.sort(function(a, b){return a - b});
-    //console.log(brr);
+	
 	while(brr.length>1){
 	brr.sort(function(a, b){return a - b});
-        console.log(brr);
+      
 		var a=brr.shift();
 		var b=brr.shift();
 		var c=a+b;
         crr.push(c);
-        //console.log(c);
+       
 		brr.unshift(c);
         
 	}
 	
-	console.log(brr);
-    typeof(brr);
+	
     var count=0;
     for(let i=0;i<crr.length;i++){
     count+=crr[i];
     }
-console.log(count);
+
 	document.getElementById('result').innerHTML=count;
   
 } 
