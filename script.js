de3b31f0d2b570eda010e4bf1c7782709cbec0bb
bhargavsai888 <117674796+bhargavsai888@users.noreply.github.com>
2023-07-12 15:18:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
+<script>
 function calculateMinCost() {
   //your code here
 	var str=document.getElementById('rope-lengths').value;
   
   var arr=str.split(',');
-	while(arr.length>1){
-		arr.sort();
-		var a=arr.shift();
-		var b=arr.shift();
-		var c=a+b;
-		arr.unshift(c);
-	}
-	var final=arr.join();
-	console.log(final);
-	document.getElementById('result').value=final;
   
-}  
+  var brr=arr.map(function(str){
+  return parseInt(str);
+  });
+  var crr=[];
+	//brr.sort(function(a, b){return a - b});
+    //console.log(brr);
+	while(brr.length>1){
+	brr.sort(function(a, b){return a - b});
+        console.log(brr);
+		var a=brr.shift();
+		var b=brr.shift();
+		var c=a+b;
+        crr.push(c);
+        //console.log(c);
+		brr.unshift(c);
+        
+	}
+	
+	console.log(brr);
+    typeof(brr);
+    var count=0;
+    for(let i=0;i<crr.length;i++){
+    count+=crr[i];
+    }
+console.log(count);	document.getElementById('result').innerHTML=count;
+  
+} 
+
+</script>
